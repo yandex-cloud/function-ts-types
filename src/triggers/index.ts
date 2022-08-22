4a1d6e9af29c089091dd3ceb9f61bd3493b665e4
jsus <esseswann@gmail.com>
2022-08-22 11:01:28 +0300

    Container registry added

--- src/triggers/index.ts
+++ src/triggers/index.ts
@@ -1,3 +1,4 @@
+import type ContainerRegistryExample from "./containerRegistry.json";
 import type MessageQueueExample from "./messageQueue.json";
 import type ObjectStorageExample from "./objectStorage.json";
 import type TimerExample from "./timer.json";
@@ -5,3 +6,4 @@ import type TimerExample from "./timer.json";
 export type Timer = typeof TimerExample;
 export type MessageQueue = typeof MessageQueueExample;
 export type ObjectStorage = typeof ObjectStorageExample;
+export type ContainerRegistry = typeof ContainerRegistryExample;
